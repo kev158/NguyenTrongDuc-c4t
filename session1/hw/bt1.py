42def3d1cec59d5a0b0b313416dbf6920722d0df
kev158 <trongduc811@gmail.com>
2019-01-27 21:35:15 +0700

    dasdasd

---
+++ session1/hw/bt1.py
@@ -0,0 +1,4 @@
+x=input("Hi there, what is your firstname?")
+y=input("Hi there, what is your lastname?")
+
+print ("Your fullname ", x, y)
---
+++ session1/test.py
@@ -0,0 +1,19 @@
+# x=int(input ("Nhap vap nam sinh cua minh:"))
+# y = 2018 - x
+# z = 2019 - x
+
+# print (y, z)
+
+# x=int(input ("Nhap vao ban kinh hinh tron:"))
+# y = 3.14 * x**2
+# print (y)
+
+# x=int(input("Nhap vao do dai chieu cao:"))
+# y=int(input("Nhap vao do dai canh day:"))
+# z = (x*y)/2
+# print (z)
+
+x = "2002"
+y = 2019
+z = y - x
+print(z)
